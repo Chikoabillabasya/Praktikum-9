25bb5ceaefb729c72a5010d21afdff1ace585818
Chikoabillabasya <galangrizky767@gmail.com>
2023-10-23 12:12:09 +0700

    Array Nilai

--- ArrayNilai07.java
+++ ArrayNilai07.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 public class ArrayNilai07 {
     public static void main(String[] args) {
+        Scanner input07 = new Scanner (System.in);
 
         int[] nilaiAkhir = new int [10];
 
         for (int i = 0; i < 10; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
-            nilaiAkhir[i] = sc.nextInt();
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiakhir[i]);
-            
+            nilaiAkhir[i] = input07.nextInt();
         }
-        } 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
     }
 }
